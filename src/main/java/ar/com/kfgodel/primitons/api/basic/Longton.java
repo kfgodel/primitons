5555c0202b60b2ccd2e27130a90f9aa9a43c8a54
Dario L Garcia <dario.garcia@10pines.com>
2017-08-04 22:57:43 -0300

    Modify long to int tests to remove arbitrary exceptions

--- src/main/java/ar/com/kfgodel/primitons/api/basic/Longton.java
+++ src/main/java/ar/com/kfgodel/primitons/api/basic/Longton.java
@@ -1,8 +1,6 @@
 package ar.com.kfgodel.primitons.api.basic;
 
-import ar.com.kfgodel.primitons.api.exceptions.OverflowException;
 import ar.com.kfgodel.primitons.api.exceptions.PrimitonException;
-import ar.com.kfgodel.primitons.api.exceptions.UnderflowException;
 
 /**
  * This type defines the primiton functions for the primitive long type
@@ -10,12 +8,6 @@ import ar.com.kfgodel.primitons.api.exceptions.UnderflowException;
 public interface Longton {
 
   static int toInt(long input) throws PrimitonException {
-    if(input > Integer.MAX_VALUE){
-      throw new OverflowException(input, int.class);
-    }
-    if(input < Integer.MIN_VALUE){
-      throw new UnderflowException(input, int.class);
-    }
     return (int) input;
   }
 
--- src/test/java/ar/com/kfgodel/primitons/numeric/Long2IntTest.java
+++ src/test/java/ar/com/kfgodel/primitons/numeric/Long2IntTest.java
@@ -4,8 +4,6 @@ import ar.com.dgarcia.javaspec.api.JavaSpec;
 import ar.com.dgarcia.javaspec.api.JavaSpecRunner;
 import ar.com.kfgodel.primitons.PrimitonTestContext;
 import ar.com.kfgodel.primitons.api.basic.Longton;
-import ar.com.kfgodel.primitons.api.exceptions.OverflowException;
-import ar.com.kfgodel.primitons.api.exceptions.UnderflowException;
 import org.junit.runner.RunWith;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -29,26 +27,24 @@ public class Long2IntTest extends JavaSpec<PrimitonTestContext> {
           assertThat(context().intOutput()).isEqualTo(23);
         });
 
-        it("converts the maximum int value",()->{
+        it("converts the maximum int value without truncation",()->{
           context().longInput(()-> 2147483647L);
           assertThat(context().intOutput()).isEqualTo(Integer.MAX_VALUE);
         });
 
-        it("converts the minimum int value",()->{
+        it("converts the minimum int value without truncation",()->{
           context().longInput(()-> -2147483648L);
           assertThat(context().intOutput()).isEqualTo(Integer.MIN_VALUE);
         });
-
-        itThrows(OverflowException.class, "if value is too big for an int",()->{
-         context().long2int().apply(2147483648L);
-        }, e ->{
-          assertThat(e).hasMessage("Value[2147483648] overflows type [int]");
+        
+        it("converts the any value bigger than maximum int with truncation",()->{
+          context().longInput(()-> Long.MAX_VALUE);
+          assertThat(context().intOutput()).isEqualTo(-1); //Most significant bits are lost
         });
 
-        itThrows(UnderflowException.class, "if value is too small for an int",()->{
-          context().long2int().apply(-2147483649L);
-        }, e ->{
-          assertThat(e).hasMessage("Value[-2147483649] underflows type [int]");
+        it("converts the any value smaller than minimum int with truncation",()->{
+          context().longInput(()-> Long.MIN_VALUE);
+          assertThat(context().intOutput()).isEqualTo(0); //Most significant bits are lost
         });
 
       });
